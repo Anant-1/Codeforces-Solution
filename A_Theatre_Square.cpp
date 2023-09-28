fc8c3e99f782a1ed1e6f15d4fbe768b58a398d79
Anant-1 <54532485+Anant-1@users.noreply.github.com>
2023-09-28 13:34:03 +0530

    1A

---
+++ A_Theatre_Square.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+
+int main() {
+    int n, m, a;
+    cin >> n >> m >> a;
+    ll x = n/a, y = m/a;
+    if(n % a != 0) {
+        x++;
+    }
+    if(m % a != 0) {
+        y++;
+    }
+    cout << x*y;
+    return 0;
+}
+
+
+
+
